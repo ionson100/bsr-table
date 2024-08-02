f55d44d47551f32c6210372412efbb769afc0162
123 <123@DESKTOP-2UK3QN5>
2024-08-02 17:17:10 +0200

    + 23.23

--- dist/esm/table/Column.d.ts
+++ dist/esm/table/Column.d.ts
@@ -4,4 +4,5 @@ export declare class Column extends React.Component<PropsColumn, any> {
     constructor({ props }: {
         props: Readonly<PropsColumn>;
     });
+    render(): undefined;
 }
--- dist/esm/table/Table.d.ts
+++ dist/esm/table/Table.d.ts
@@ -3,14 +3,17 @@ import { PropsTable } from "./PropsTable";
 export declare class Table extends React.Component<PropsTable, any> {
     private list;
     private id?;
+    private listGroup;
+    private listHeaderGroup;
     constructor({ props }: {
         props: Readonly<PropsTable>;
     });
     innerRender(): void;
+    private innerParserProps;
     columnClick(column: number): void;
     cellClick(row: number, column: number): void;
     rowClick(row: number): void;
     Refresh(callback: () => void): void;
-    componentDidMount(): void;
+    renderHeaderGroup(): React.JSX.Element | null | undefined;
     render(): React.JSX.Element;
 }
